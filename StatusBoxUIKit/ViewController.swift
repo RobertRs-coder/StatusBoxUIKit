4d4debe3d7bbf5c6ff63723c2232c58b982b7c52
Roberto Rojo <rrojo.va@gmail.com>
2022-11-03 05:15:01 +0100

    Initial Commit

---
+++ StatusBoxUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StatusBoxUIKit
+//
+//  Created by Roberto Rojo Sahuquillo on 3/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
